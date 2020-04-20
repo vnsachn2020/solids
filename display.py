fa5c18d55609778a255a727fe626d1d5614c0679
Vanessa Chen <vchen00@stuy.edu>
2020-04-20 07:52:14 -0400

    finish

--- display.py
+++ display.py
@@ -29,8 +29,9 @@ def new_zbuffer( width = XRES, height = YRES ):
 
 def plot( screen, zbuffer, color, x, y, z ):
     newy = YRES - 1 - y
-    if ( x >= 0 and x < XRES and newy >= 0 and newy < YRES ):
+    if ( x >= 0 and x < XRES and newy >= 0 and newy < YRES and zbuffer[x][y] <= z):
         screen[newy][x] = color[:]
+        zbuffer[newy][y] = z
 
 def clear_screen( screen ):
     for y in range( len(screen) ):
